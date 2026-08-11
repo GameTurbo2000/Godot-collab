edbfe889e79a0e7d2a0a7a353db51f7d572620e5
GameTurbo2000 <lakatosmarcell447@gmail.com>
2026-08-11 08:35:14 +0200

    jobb coinok

--- Scripts/coin.gd
+++ Scripts/coin.gd
@@ -1,7 +1,15 @@
 extends Area2D
 
 @onready var game_manager: Node = %GameManager
+@onready var coin: Area2D = $"."
+@onready var timer: Timer = $Timer
+
 
 func _on_body_entered(body: Node2D) -> void:
 	game_manager.add_point()
+	coin.position.y -= 10
+	timer.start()
+
+
+func _on_timer_timeout() -> void:
 	queue_free()
